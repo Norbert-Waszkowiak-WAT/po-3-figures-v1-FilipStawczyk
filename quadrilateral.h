60da9bdb9093f88af0f20e74ff6b0126b88e8d9d
FilipStawczyk <163164320+FilipStawczyk@users.noreply.github.com>
2024-12-02 09:07:08 +0000

    czworokąt

---
+++ quadrilateral.h
@@ -0,0 +1,22 @@
+#ifndef QUADRILATERAL_H
+#define QUADRILATERAL_H
+#include <string>
+#include "point.h"
+using namespace std;
+
+class Quadrilateral{
+private:
+    Point a;
+    Point b;
+    Point c;
+    Point d;
+public:
+    Quadrilateral(Point a, Point b, Point c, Point d);
+    Quadrilateral(Quadrilateral &other);
+    bool equals(Quadrilateral &other);
+    string toString();
+    void flip();
+    void move(double p, double g);
+    double getSurface();
+};
+#endif
